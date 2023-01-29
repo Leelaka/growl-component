d761a1e983016d7026abccfd33d903524bb60964
Leelaka Adassuriya <37642488+Leelaka@users.noreply.github.com>
2023-01-30 03:35:12 +1100

    turning growl component into a npm lib

---
+++ src/growl.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+
+import "./growl.css";
+
+export const Growl = ({ active, message, onDismissed }) => (
+  <div className={`growl${active ? " active" : ""}`}>
+    {message}
+    <div onClick={onDismissed} className="growl-close" />
+  </div>
+);
+
+export function useGrowl(dismissAfter = 3000) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false);
+
+  useEffect(() => {
+    if (growlActive) {
+      const timeout = setTimeout(() => {
+        setGrowlActive(false);
+      }, dismissAfter);
+      return () => clearTimeout(timeout);
+    }
+  }, [growlActive, dismissAfter]);
+
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+    },
+  ];
+}
+
+export default Growl;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "Growl",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+  },
+};
